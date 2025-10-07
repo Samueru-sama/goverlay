aff174c6b00a03f4e7f0abc338b25e0daf2e3deb
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-06 20:26:18 -0400

    wtf

--- appimage/get-dependencies.sh
+++ appimage/get-dependencies.sh
@@ -54,7 +54,8 @@ pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-pacman -Syu --noconfirm pascube
+pacman -Syu --noconfirm
+pacman -S --noconfirm pascube
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
